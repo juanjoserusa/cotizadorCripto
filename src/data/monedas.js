742352e326acb21aa0c315b120fc619adb1f8475
Juan Jose Ruiz Sanchez <juanjoserusa@gmail.com>
2023-02-14 13:26:23 +0100

    aplicacion creada

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    {id: 'EUR', nombre: 'Euros'},
+    {id: 'USD', nombre: 'Dolar EEUU'}, 
+    {id: 'GBP', nombre: 'Libras Esterlina'}, 
+    {id: 'MXN', nombre: 'Peso Mexicano'},
+
+]
+
+export{
+    monedas
+}
